08a01f78dba9d86447f0c1eced1b17cd47ee4ef0
iamraidor <aleturovichyevgeny@gmail.com>
2026-04-14 23:52:32 +0300

    Create concrete buttons creator classes

---
+++ Calculator/Scripts/NumberButtonCreator.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Calculator.Scripts;
+
+public class NumberButtonCreator : ButtonCreator
+{
+    public override Button CreateButton(string content)
+    {
+        return new Button
+        {
+            Content = content,
+            FontSize = 18,
+            Margin = new Thickness(5)
+        };
+    }
+}
---
+++ Calculator/Scripts/OperationButtonCreator.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Calculator.Scripts;
+
+public class OperationButtonCreator : ButtonCreator
+{
+    public override Button CreateButton(string content)
+    {
+        return new Button
+        {
+            Content = content,
+            FontSize = 18,
+            Margin = new Thickness(5),
+            Background = System.Windows.Media.Brushes.LightGray
+        };
+    }
+}
